b97990f35ec28731870528f3c4826ea493e114dc
Mehdy Asaad <151958475+Mehdy-H-Asaad@users.noreply.github.com>
2024-03-11 06:32:09 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+import { Suspense, lazy } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import AboutPage from "./Pages/AboutPage";
-import MenuPage from "./Pages/MenuPage";
-import Contact from "./Pages/Contact";
-import ItemInfo from "./Sections/ItemInfo";
+import { Spinner } from "./GlobalComponents/GlobalComponents";
+
+// CODE SPLITTING IMPLEMENTATION
+const Home = lazy(() => import("./Pages/Home"));
+const AboutPage = lazy(() => import("./Pages/AboutPage"));
+const MenuPage = lazy(() => import("./Pages/MenuPage"));
+const Contact = lazy(() => import("./Pages/Contact"));
+const ItemInfo = lazy(() => import("./Sections/ItemInfo"));
 
 function App() {
 	return (
 		<HashRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/menu" element={<MenuPage />} />
-				<Route path="/about" element={<AboutPage />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/item/:cuisine/:itemId" element={<ItemInfo />} />
-			</Routes>
+			<Suspense fallback={<Spinner></Spinner>}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/menu" element={<MenuPage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/item/:cuisine/:itemId" element={<ItemInfo />} />
+				</Routes>
+			</Suspense>
 		</HashRouter>
 	);
 }
